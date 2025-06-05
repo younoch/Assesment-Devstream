bd5d7e92dcb55906a7408cccd2d887919e46e517
younoch <younoch295@outlook.com>
2025-06-05 23:18:24 +0600

    remove toast

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -4,7 +4,6 @@ import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import apiClient from '../../api/client';
 import type { LoginCredentials } from './types/auth';
-import { useToast } from '@/composables/useToast';
 
 export const useAuthStore = defineStore('auth', () => {
   const router = useRouter();
@@ -40,7 +39,6 @@ export const useAuthStore = defineStore('auth', () => {
       const res = await apiClient.post('/token/', credentials, { withCredentials: true });
       
       setTokens(res.data.data.access_token, res.data.data.refresh_token);
-      useToast().success('Login successful!');
     } catch (err: any) {
       error.value = err.response?.data?.detail || 'Login failed';
       throw err;
